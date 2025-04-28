3c9b0fcfb7ca0e63f975effa63ea86b5bdb88ccc
Pedro-5D <pdamborenea@gmail.com>
2025-04-29 00:07:05 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,37 @@
+import http.server
+import socketserver
+import os
+
+# Puerto por defecto para Render.com o puerto 8000 localmente
+PORT = int(os.environ.get('PORT', 8000))
+
+# Directorio actual como directorio de servicio
+DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
+# Lista de archivos permitidos para descarga directa
+ALLOWED_FILES = ['de421.bsp']
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+    
+    def end_headers(self):
+        # Agregamos CORS headers para permitir pruebas locales
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET')
+        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
+        super().end_headers()
+    
+    def do_GET(self):
+        # Servir archivos normales
+        return super().do_GET()
+
+# Crear el servidor
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Servidor iniciado en http://localhost:{PORT}")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nServidor detenido por el usuario")
+    finally:
+        httpd.server_close()
